18f5543598617b0620899ff208375db68906f923
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2024-07-23 14:06:06 -0300

    Update manageSubscription.ts

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,7 +1,7 @@
 import { query as q } from 'faunadb';
 
 import { fauna } from '../../../services/fauna';
-import { stripe } from '../../../services/stripe';
+//import { stripe } from '../../../services/stripe';
 
 export async function saveSubscription(
   subscriptionId: string,
@@ -23,7 +23,7 @@ export async function saveSubscription(
     )
   )
 
-  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+  const subscription = ""//await stripe.subscriptions.retrieve(subscriptionId)
 
   const subscriptionData = {
     id: subscription.id,
