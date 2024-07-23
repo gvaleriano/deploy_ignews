5bda192d9c3603740f3a8f699465c8c104f15469
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2024-07-23 14:20:52 -0300

    Update manageSubscription.ts

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -22,7 +22,73 @@ export async function saveSubscription(
     )
   )
 
-  const subscription = ""//await stripe.subscriptions.retrieve(subscriptionId)
+  const subscription = { id: "sub_1MowQVLkdIwHu7ixeRlqHVzs", "status": "active"   "items": {
+    "object": "list",
+    "data": [
+      {
+        "id": "si_Na6dzxczY5fwHx",
+        "object": "subscription_item",
+        "billing_thresholds": null,
+        "created": 1679609768,
+        "metadata": {},
+        "plan": {
+          "id": "price_1MowQULkdIwHu7ixraBm864M",
+          "object": "plan",
+          "active": true,
+          "aggregate_usage": null,
+          "amount": 1000,
+          "amount_decimal": "1000",
+          "billing_scheme": "per_unit",
+          "created": 1679609766,
+          "currency": "usd",
+          "discounts": null,
+          "interval": "month",
+          "interval_count": 1,
+          "livemode": false,
+          "metadata": {},
+          "nickname": null,
+          "product": "prod_Na6dGcTsmU0I4R",
+          "tiers_mode": null,
+          "transform_usage": null,
+          "trial_period_days": null,
+          "usage_type": "licensed"
+        },
+        "price": {
+          "id": "price_1MowQULkdIwHu7ixraBm864M",
+          "object": "price",
+          "active": true,
+          "billing_scheme": "per_unit",
+          "created": 1679609766,
+          "currency": "usd",
+          "custom_unit_amount": null,
+          "livemode": false,
+          "lookup_key": null,
+          "metadata": {},
+          "nickname": null,
+          "product": "prod_Na6dGcTsmU0I4R",
+          "recurring": {
+            "aggregate_usage": null,
+            "interval": "month",
+            "interval_count": 1,
+            "trial_period_days": null,
+            "usage_type": "licensed"
+          },
+          "tax_behavior": "unspecified",
+          "tiers_mode": null,
+          "transform_quantity": null,
+          "type": "recurring",
+          "unit_amount": 1000,
+          "unit_amount_decimal": "1000"
+        },
+        "quantity": 1,
+        "subscription": "sub_1MowQVLkdIwHu7ixeRlqHVzs",
+        "tax_rates": []
+      }
+    ],
+    "has_more": false,
+    "total_count": 1,
+    "url": "/v1/subscription_items?subscription=sub_1MowQVLkdIwHu7ixeRlqHVzs"
+  }}//await stripe.subscriptions.retrieve(subscriptionId)
 
   const subscriptionData = {
     id: subscription.id,
